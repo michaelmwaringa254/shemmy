fc2b019961d2f696ddb651540684e848c2a4574f
Michaelmwaringa <acadeemiasms@gmail.com>
2025-08-07 13:23:33 +0300

    Updated About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -108,8 +108,8 @@ const About = () => {
                     <p className="text-orange-100">Client Satisfaction</p>
                   </div>
                 </div>
-                <div></div>
               </div>
+              <div></div>
             </div>
           </div>
         </div>
